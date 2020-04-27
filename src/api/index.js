3c1c1e0d3501562b9b14ff42d8a63656d1af1281
lupenq <skinilya@gmail.com>
2020-04-28 00:17:29 +0300

    Create chart bar

--- src/api/index.js
+++ src/api/index.js
@@ -16,3 +16,21 @@ export const fetchData = async () => {
     console.log(e)
   }
 }
+
+export const fetchDailyData = async () => {
+  try {
+
+    const { data } = await axios.get(`${url}/daily`)
+
+    const modifiedData = data.map((dailyData) => ({
+      confirmed: dailyData.confirmed.total,
+      deaths: dailyData.deaths.total,
+      date: dailyData.reportDate
+    }))
+
+    return modifiedData
+
+  } catch (e) {
+    console.log(e)
+  }
+}
