1b049b37025ad4bfb69fdc6ea7d454fd8e71f72c
lupenq <skinilya@gmail.com>
2020-04-28 23:04:42 +0300

    Create logo

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import React from 'react'
 import { Cards, Chart, CountryPicker} from './components'
 import styles from './App.module.css'
 import {fetchData, fetchTodayByCountryData, fetchTodayData} from './api'
+import {Typography} from '@material-ui/core'
 
-import coronaImage from './images/image.png'
+import microbe from './images/microbe.svg'
+
 
 class App extends React.Component {
   state = {
@@ -34,7 +36,7 @@ class App extends React.Component {
 
     return (
       <div className={styles.container}>
-        <img className={styles.image} src={coronaImage} alt="COVID-19"/>
+        <Typography variant="h1" align="center" className={styles.logo}>C<img src={microbe} className={styles.image} alt="COVID-19"/>VID-19</Typography>
         <Cards data={data} />
         <CountryPicker data={data} handleCountryChange={this.handleCountryChange} />
         <Chart data={data} country={country}/>
